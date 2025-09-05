3a3c33bb0c9ad0428fe6941cbb41a4ce09cd5b08
Alberto Giardino <alberto.giardino@al-ce.it>
2025-09-05 17:34:39 +0200

    [IMP] l10n_it_edi_extension: Import DataInzio/FinePeriodo

--- l10n_it_edi_extension/models/account_move.py
+++ l10n_it_edi_extension/models/account_move.py
@@ -586,6 +586,14 @@ class AccountMoveInherit(models.Model):
             # Admin. ref.
             if admin_ref := get_text(element, ".//RiferimentoAmministrazione"):
                 move_line.l10n_it_edi_admin_ref = admin_ref
+            # Deferred dates
+            period_start = get_date(element, ".//DataInizioPeriodo")
+            period_end = get_date(element, ".//DataFinePeriodo")
+            if period_start and period_end:
+                move_line.write({
+                    'deferred_start_date': period_start,
+                    'deferred_end_date': period_end,
+                })
 
             vals = {
                 "line_number": int(get_text(element, ".//NumeroLinea")),
