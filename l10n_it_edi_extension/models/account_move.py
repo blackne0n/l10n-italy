6394b1d235ed81ca69d6b6fc9bc5f099edc5784e
Alberto Giardino <alberto.giardino@al-ce.it>
2025-09-07 11:18:47 +0200

    [IMP] l10n_it_edi_extension: Use product_customerinfo code

--- l10n_it_edi_extension/models/account_move.py
+++ l10n_it_edi_extension/models/account_move.py
@@ -208,11 +208,21 @@ class AccountMoveInherit(models.Model):
         )
         for base_line, _aggregated_values in base_lines_aggregated_values:
             line = base_line["record"]
+            partner = line.partner_id or self.partner_id
+            domain = [
+                ('product_tmpl_id', '=', line.product_id.product_tmpl_id.id),
+                ('partner_id', '=', partner.id),
+            ]
+            if line.product_id.product_variant_count > 1:
+                domain.append(('product_id', '=', line.product_id.id))
+            info = self.env['product.customerinfo'].search(domain, limit=1)
+            product_code = info.product_code or None
             base_line["it_values"].update(
                 {
                     "admin_ref": line.l10n_it_edi_admin_ref or None,
                     "period_start_date": line.deferred_start_date or None,
                     "period_end_date": line.deferred_end_date or None,
+                    "partner_product_ref": product_code,
                 }
             )
         return res
